33eb13ebdd766d34a886beec40c2fd5ccb3de458
kano4 <shinji.warp@gmail.com>
2012-01-13 20:22:12 +0900

    add .emacs.d/init.el and remove .emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,8 @@
+;; don't make backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+;; dont't show startup screen
+(setq inhibit-startup-screen t)
+
+;; remove trailing whitespace before saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
